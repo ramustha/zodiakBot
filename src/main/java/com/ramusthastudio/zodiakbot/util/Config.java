fa08788252b3797dba772aa1f75a79894da3748e
ramustha nest <ramustha nest>
2017-03-26 13:28:39 +0700

    try to fixing

--- src/main/java/com/ramusthastudio/zodiakbot/util/Config.java
+++ src/main/java/com/ramusthastudio/zodiakbot/util/Config.java
@@ -1,14 +1,20 @@
 package com.ramusthastudio.zodiakbot.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 public class Config {
+  final
+  Environment mEnv;
+  @Autowired public Config(Environment mEnv) {this.mEnv = mEnv;}
+
   @Bean(name = "line.bot.channelSecret")
-  public String getChannelSecret() { return System.getenv("line.bot.channelSecret"); }
+  public String getChannelSecret() { return mEnv.getProperty("line.bot.channelSecret"); }
   @Bean(name = "line.bot.channelToken")
-  public String getChannelAccessToken() { return System.getenv("line.bot.channelToken"); }
+  public String getChannelAccessToken() { return mEnv.getProperty("line.bot.channelToken"); }
 }
