5fc2f746f751b1b561cfe2e77a16490ea82e814d
ramustha nest <ramustha nest>
2017-03-27 20:54:13 +0700

    fixing

--- src/main/java/com/ramusthastudio/zodiakbot/util/BotHelper.java
+++ src/main/java/com/ramusthastudio/zodiakbot/util/BotHelper.java
@@ -79,7 +79,7 @@ public final class BotHelper {
         .writeTimeout(5, TimeUnit.SECONDS)
         .readTimeout(5, TimeUnit.SECONDS);
 
-    LOG.info("Starting line messaging service to 1.1...");
+    LOG.info("Starting line messaging service to SSL_TLSv2...");
     return LineMessagingServiceBuilder
         .create(aChannelAccessToken)
         .okHttpClientBuilder(enableTls12(client))
@@ -98,13 +98,13 @@ public final class BotHelper {
       }
       X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
 
-      SSLContext sslContext = SSLContext.getInstance("TLSv1.1");
+      SSLContext sslContext = SSLContext.getInstance("SSL_TLSv2");
       sslContext.init(null, new TrustManager[] {trustManager}, null);
       SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
       client.sslSocketFactory(sslSocketFactory, trustManager);
 
       ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-          .tlsVersions(TlsVersion.TLS_1_1)
+          .tlsVersions(TlsVersion.TLS_1_0)
           .build();
 
       List<ConnectionSpec> specs = new ArrayList<>();
@@ -114,7 +114,7 @@ public final class BotHelper {
 
       client.connectionSpecs(specs);
     } catch (Exception exc) {
-      LOG.error("Error while setting TLS 1.1 {}", exc.getMessage());
+      LOG.error("Error while setting TLS 1.0 {}", exc.getMessage());
     }
     return client;
   }
